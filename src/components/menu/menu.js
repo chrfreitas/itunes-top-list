b3eff69c530b5f5d8645332fb7f4951a59882f6e
Christian Freitas <chrfreitas@gmail.com>
2021-03-20 10:02:24 -0300

    Create deploy action to deploy build folder to gh-pages

---
+++ src/components/menu/menu.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+import routes from "../../routes/routes";
+import './menu.css'
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <Link className="menu__item" to={routes.topAlbums}>Top 100 Albums</Link>
+            <Link className="menu__item" to={routes.topSongs}>Top 100 Songs</Link>
+        </div>
+    )
+};
+
+export default Menu;
